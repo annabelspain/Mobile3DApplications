72494eddff4a63368922eabcedcebd432712b8ba
Annabel <as2435@sussex.ac.uk>
2021-05-13 03:30:34 +0100

    Upload of all Labs
    All of my labs in total

---
+++ Lab 8/part_c/application/model/createJson.php
@@ -0,0 +1,23 @@
+<?php
+echo '{ "users":[
+            {
+                "firstName":"Annabel",
+                "lastName":"Spain",
+                "joined": {
+                    "month":"June",
+                    "day":8,
+                    "year":1998
+                }
+            },
+            {
+                "firstName":"Molly",
+                "lastName":"Spain",
+                "joined": {
+                    "month":"October",
+                    "day":21,
+                    "year":1996
+                }
+            }
+    ]}
+	';
+?>
